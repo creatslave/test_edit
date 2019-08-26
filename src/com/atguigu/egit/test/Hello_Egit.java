8e684a8100e0ada9227711e07611cbcc9fde724e
godslave <1767498754@qq.com>
2019-08-26 17:44:10 +0800

    更新

--- src/com/atguigu/egit/test/Hello_Egit.java
+++ src/com/atguigu/egit/test/Hello_Egit.java
@@ -1,5 +1,10 @@
 package com.atguigu.egit.test;
 
 public class Hello_Egit {
+	public static void main(String[] args) {
+		System.out.println("aaa");
+		System.out.println("bbb");
+		System.out.println("xxx");
+	}
 
 }
